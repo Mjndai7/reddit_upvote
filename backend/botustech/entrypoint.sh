8b89ad022b94122c3b24d29045573ab58b943b03
Dev 0x00 <asidohsidney254@gmail.com>
2023-07-20 22:46:07 +0300

    entryfile correction

--- backend/botustech/entrypoint.sh
+++ backend/botustech/entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 python manage.py makemigrations botustech
 
 sleep 5
-python manage.py migrate &
+python manage.py migrate 
 
 sleep 5
-gunicorn botustech.wsgi:application --bind 0.0.0.0:8000 &
+gunicorn botustech.wsgi:application --bind 0.0.0.0:8000 
 
 sleep 5
 
