850627b52f824a170e95202dd911d292716929de
developer@gmail.com <developer@gmail.com>
2023-07-21 01:26:21 -0400

    recivive every request from the frontend

--- backend/botustech/reddit/backend.py
+++ backend/botustech/reddit/backend.py
@@ -22,123 +22,162 @@ for account in accounts:
 
 @app.route('/upvote', methods=['GET'])
 def upvote():
-    url = request.args.get('url')
-    threads = request.args.get('threads')
-    number_of_upvotes = request.args.get('number_of_upvotes')
-    rate = request.args.get('rate')
-    balance = request.args.get('balance')
-    semaphore = threading.Semaphore(int(threads))
-    count = 0
-    new_balance = balance
-    while True:
-        account = accounts_queue.get()
-        bot = RedditBot(account)
-        status = bot.upvote_post(url)
-        accounts_queue.task_done()
-        semaphore.release()
-        count += 1
-        if status == True:
-            new_balance = float(new_balance) - float(rate)
-        
-        if status == False:
-            new_balance = float(new_balance)
-        
-        if float(new_balance) < float(rate):
-            status = "INCOMPLETE"
-            break
+    try:
+        url = request.args.get('url')
+        threads = request.args.get('threads')
+        number_of_upvotes = request.args.get('number_of_upvotes')
+        rate = request.args.get('rate')
+        balance = request.args.get('balance')
+        semaphore = threading.Semaphore(int(threads))
+        count = 0
+        new_balance = balance
 
-        if count == int(number_of_upvotes):
-            status = "COMPLETE"
-            break
+        print(url, threads, number_of_upvotes, rate, balance, count)
+        while True:
+            account = accounts_queue.get()
+            bot = RedditBot(account)
+            status = bot.upvote_post(url)
+            accounts_queue.task_done()
+            semaphore.release()
+            count += 1
+            if status == True:
+                new_balance = float(new_balance) - float(rate)
+            
+            if status == False:
+                new_balance = float(new_balance)
+            
+            if float(new_balance) < float(rate):
+                status = "INCOMPLETE"
+                break
+
+            if count == int(number_of_upvotes):
+                status = "COMPLETE"
+                break
+        
+        response_data = {
+            'count': count,
+            'balance' : new_balance,
+            'status' : status
+            # Include any other relevant data you want to return
+        }
+        return response_data, 200   
     
-    response_data = {
-        'count': count,
-        'balance' : new_balance,
-        'status' : status
-        # Include any other relevant data you want to return
-    }
-    return response_data, 200
+    except Exception as Error:
+        print(Error)
+        response_data = {
+            'count': count,
+            'balance' : new_balance,
+            'status' : status
+            # Include any other relevant data you want to return
+        }
+        return response_data, 200 
 
 
-@app.route("/downvote/")
+@app.route("/downvote")
 def downvote():
-    url = request.args.get('url')
-    threads = request.args.get('threads')
-    number_of_downvotes = request.args.get('number_of_upvotes')
-    rate = request.args.get('rate')
-    balance = request.args.get('balance')
-    semaphore = threading.Semaphore(threads)
-    count = 0
-    new_balance = float(balance)
-    while True:
-        account = accounts_queue.get()
-        bot = RedditBot(account)
-        status = bot.downvote_post(url)
-        accounts_queue.task_done()
-        semaphore.release()
-        count += 1
-        if status == True:
-            new_balance = float(new_balance) - float(rate)
-        
-        if status == False:
-            new_balance = float(new_balance)
-        
-        if float(new_balance) < float(rate):
-            status = "INCOMPLETE"
-            break
+    try:
+        url = request.args.get('url')
+        threads = request.args.get('threads')
+        number_of_upvotes = request.args.get('number_of_downvotes')
+        rate = request.args.get('rate')
+        balance = request.args.get('balance')
+        semaphore = threading.Semaphore(int(threads))
+        count = 0
+        new_balance = balance
 
-        if count == int(number_of_downvotes):
-            status = "COMPLETE"
-            break
+        print(url, threads, number_of_upvotes, rate, balance, count)
+        while True:
+            account = accounts_queue.get()
+            bot = RedditBot(account)
+            status = bot.downvote_post(url)
+            accounts_queue.task_done()
+            semaphore.release()
+            count += 1
+            if status == True:
+                new_balance = float(new_balance) - float(rate)
+            
+            if status == False:
+                new_balance = float(new_balance)
+            
+            if float(new_balance) < float(rate):
+                status = "INCOMPLETE"
+                break
+
+            if count == int(number_of_upvotes):
+                status = "COMPLETE"
+                break
+        
+        response_data = {
+            'count': count,
+            'balance' : new_balance,
+            'status' : status
+            # Include any other relevant data you want to return
+        }
+        return response_data, 200   
     
-    response_data = {
-        'count': count,
-        'balance' : new_balance,
-        'status' : status
-        # Include any other relevant data you want to return
-    }
-    return response_data, 200
+    except Exception as Error:
+        print(Error)
+        response_data = {
+            'count': count,
+            'balance' : new_balance,
+            'status' : status
+            # Include any other relevant data you want to return
+        }
+        return response_data, 200 
 
 @app.route("/comment/")
 def comment():
-    url = request.args.get('url')
-    threads = request.args.get('threads')
-    number_of_comments = request.args.get('number_of_upvotes')
-    rate = request.args.get('rate')
-    balance = request.args.get('balance')
-    comment = request.args.get("comment")
-    semaphore = threading.Semaphore(threads)
-    count = 0
-    new_balance = balance
-    while True:
-        account = accounts_queue.get()
-        bot = RedditBot(account)
-        bot.comment_post(url, comment=comment)
-        accounts_queue.task_done()
-        semaphore.release()
-        count += 1
-        if status == True:
-            new_balance = new_balance - rate
-        
-        if status == False:
-            new_balance = new_balance
-        
-        if new_balance < rate:
-            status = "INCOMPLETE"
-            break
+    try:
+        url = request.args.get('url')
+        threads = request.args.get('threads')
+        number_of_upvotes = request.args.get('number_of_comments')
+        rate = request.args.get('rate')
+        balance = request.args.get('balance')
+        comment = request.args.get("comment")
+        semaphore = threading.Semaphore(int(threads))
+        count = 0
+        new_balance = balance
 
-        if count == number_of_comments:
-            status = "COMPLETE"
-            break
+        print(url, threads, number_of_upvotes, rate, balance, count)
+        while True:
+            account = accounts_queue.get()
+            bot = RedditBot(account)
+            status = bot.comment_post(url, comment)
+            accounts_queue.task_done()
+            semaphore.release()
+            count += 1
+            if status == True:
+                new_balance = float(new_balance) - float(rate)
+            
+            if status == False:
+                new_balance = float(new_balance)
+            
+            if float(new_balance) < float(rate):
+                status = "INCOMPLETE"
+                break
+
+            if count == int(number_of_upvotes):
+                status = "COMPLETE"
+                break
+        
+        response_data = {
+            'count': count,
+            'balance' : new_balance,
+            'status' : status
+            # Include any other relevant data you want to return
+        }
+        return response_data, 200   
     
-    response_data = {
-        'count': count,
-        'balance' : new_balance,
-        'status' : status
-        # Include any other relevant data you want to return
-    }
-    return response_data, 200
+    except Exception as Error:
+        print(Error)
+        response_data = {
+            'count': count,
+            'balance' : new_balance,
+            'status' : status
+            # Include any other relevant data you want to return
+        }
+        return response_data, 200 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
